108417b8fec58e57d301af6c7dd3eb645130ff18
Adam Baker <bakerac4@gmail.com>
2019-08-13 15:09:29 +0000

    Adding passed attributes to the linkto component

--- dist/src/glimmer/components/LinkTo/template.js
+++ dist/src/glimmer/components/LinkTo/template.js
@@ -1,10 +1,10 @@
 import { precompile } from '@glimmer/compiler';
 export default precompile(`
 {{#if (has-block)}}
-    <FlexboxLayout class="link-to" {{on 'tap' (action this.onClick)}}>
+    <FlexboxLayout ...attributes class="link-to" {{on 'tap' (action this.onClick)}}>
         {{yield}}
     </FlexboxLayout>
 {{else}}
-    <button text={{@text}} class="btn link-to" {{on 'tap' (action this.onClick)}}></button>
+    <button ...attributes text={{@text}} class="btn link-to" {{on 'tap' (action this.onClick)}}></button>
 {{/if}}
 `);
